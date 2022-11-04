2840eecb3b48f074b80f41363335435690c235d0
knakna <kna@tut.by>
2022-11-04 20:43:19 +0300

    add route

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import s from './Navbar.module.scss'
+import {NavLink} from 'react-router-dom';
 
 
 export function Navbar() {
     return (
         <nav className={s.navbar}>
             <ul>
-                <li><a>Profile</a></li>
-                <li><a>Messages</a></li>
-                <li><a>News</a></li>
-                <li><a>Music</a></li>
-                <li><a>Settings</a></li>
+                <li><NavLink to='/profile'>Profile</NavLink></li>
+                <li><NavLink to='/dialogs'>Messages</NavLink></li>
+                <li><NavLink to='/news'>News</NavLink></li>
+                <li><NavLink to='/music'>Music</NavLink></li>
+                <li><NavLink to='/settings'>Settings</NavLink></li>
             </ul>
         </nav>
     );
